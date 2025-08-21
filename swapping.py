7c5df657061ca32bf43b15fb5c3da0915bb5b798
krishanthecoder93 <krishan.00786@gmail.com>
2025-08-21 14:29:33 +0530

    swapping of variable

---
+++ swapping.py
@@ -0,0 +1,18 @@
+'''#without using any variable
+a = int(input("Enter a number 'a': "))
+b = int(input("Enter a other number 'b' : "))
+#swapping
+a,b = b,a
+print(f"a= {a}")
+print(f"b={b}")'''
+
+'''# using third variable
+a = int(input("Enter a number 'a': "))
+b = int(input("Enter a other number 'b' : "))
+# swapping
+temp=a
+a=b
+b=temp
+
+print(f"a= {a}")
+print(f"b={b}")'''
